17bdacbed14c1d526a4525483fbabfbe97becdd7
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-06-12 21:40:11 -0400

    seed.sql added

---
+++ config/db/seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO owners(name, email)  
+	VALUES  ('Brandon Stevenson', 'test1@class.com'), 
+			('Ben Clark', 'test2@email.com'), 
+            ('Matt Brooks', 'test3@email.com'),
+			('Peter Lau', 'test4@email.com');
+SELECT * FROM dogs;
+
+INSERT INTO dogs(name, breed, size, picture, personality, age)
+	VALUES ('Sadie', 'Labrador Retreiver', 3, 'blank.png', 1, 3),
+    ('Roscoe', 'Pitbull', 3, 'blank.png', 1, 2),
+    ('Merc', 'Chi-weenie', 1, 'blank.png', 1, 2),
+    ('Marshall', 'German Shepherd', 1, 'blank.png', 1, 2),
+    ('Ella', 'Moxie', 1, 'blank.png', 1, 1),
+    ('Lola', 'Poodle', 1, 'blank.png', 3, 3),
+    ('Sebastian', 'Yorkie', 1, 'blank.png', 1, 3)
+	
+	# Size- 1:small 2:medium 3:large
+    # Personality- 1:calm 2:timed: 3:energetic
+    # Age- 1:0-5 2:6-10 3:11+
