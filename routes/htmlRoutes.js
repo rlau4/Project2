87d78887da0f5de936d8a31c9ebd872ad9d21f2d
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-06-15 10:44:55 -0400

    ; after tables in schema.sql

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,10 +3,10 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Dog.findAll({}).then(function(dbExamples) {
+    db.Dog.findAll({}).then(function(dbDog) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: dbDog
       });
     });
   });
