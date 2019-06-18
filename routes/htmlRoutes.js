3f2198db1fa09a7afeb71cb194bcfd1db8998290
MattBrooks93 <46873343+MattBrooks93@users.noreply.github.com>
2019-06-17 20:49:23 -0400

    pre pull commit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,15 +16,9 @@ module.exports = function(app) {
     res.render("login");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load dashboard
+  app.get("/dashboard", function(req, res) {
+    res.render("dashboard");
   });
 
   // Render 404 page for any unmatched routes
