01cc444b8115b75d5b0c0d054c07578bc43ed0eb
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-06-19 18:58:43 -0400

    updated schems/seed files, changed dog size's on dashboard options and made 'energetic' personality 3, changed insane to timid to match legend in seed.sql, changed sql to show weird as personality option

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
-/* Starter Owner Data */
+# Starter Owner Data 
 
 INSERT INTO owners(name, email, pic)  
 	VALUES  ('Brandon Stevenson', '1234@test.com', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQS_jpBJb4RJi0o0kWHjEIY0yX-iQLxI7xEOWt-8bzLX81H5s3GEw'),
@@ -8,7 +8,7 @@ INSERT INTO owners(name, email, pic)
 
 
 
-/*Starter Dog Date */
+#Starter Dog Date 
 
 INSERT INTO dogs(dogName, breed, size, pic, personality, age)
 	VALUES ('Sadie', 'Labrador Retreiver', 3, 'https://i.pinimg.com/originals/29/0c/19/290c192b887d0419bf8d686290bcb01b.jpg', 1, 3),
@@ -17,8 +17,8 @@ INSERT INTO dogs(dogName, breed, size, pic, personality, age)
     ('Marshall', 'German Shepherd', 1, 'http://cdn.akc.org/content/hero/gsd_header_.jpg?cachebuster:56', 1, 2),
     ('Ella', 'Moxie', 1, 'https://i.pinimg.com/originals/11/e7/c9/11e7c969d7d8583ce09a3248e4d10566.jpg', 1, 1),
     ('Lola', 'Poodle', 1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGN0gMHB_7zVu6djPAPZ-c4wF1Htgv4h8_cY6TorPyf3xKRJ5V', 3, 3),
-    ('Sebastian', 'Yorkie', 1, 'https://1djbcc27ketw16uuv6g8izdk-wpengine.netdna-ssl.com/wp-content/uploads/2017/08/yorkshire-terrier-768x432.jpg', 1, 3)
+    ('Sebastian', 'Yorkie', 1, 'https://1djbcc27ketw16uuv6g8izdk-wpengine.netdna-ssl.com/wp-content/uploads/2017/08/yorkshire-terrier-768x432.jpg', 4, 3);
 	
 	# Size- 1:small 2:medium 3:large
-    # Personality- 1:calm 2:timid: 3:energetic
+    # Personality- 1:calm 2:timid: 3:energetic 4:weird
     # Age- 1:0-5 2:6-10 3:11+
