408c80fca77f944a7e41292512e2e047d3d4e6ac
benclark1110 <benclark1110@gmail.com>
2019-06-19 20:58:19 -0400

    ok

--- public/js/$owner.js
+++ public/js/$owner.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   var API = {
-    // getOwners: function() {
-    //   return $.ajax({
-    //     url: "api/owners",
-    //     type: "GET"
-    //   });
-    // },
+    getOwners: function() {
+      return $.ajax({
+        url: "api/owners",
+        type: "GET"
+      });
+    },
     deleteOwner: function(id) {
       return $.ajax({
         url: "api/owners/" + id,
