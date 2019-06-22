15a5eaed7f93a6677fe2a739b032e19ca701332a
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-06-22 11:00:14 -0400

    }); added to dash.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -15,23 +15,28 @@ $.get("/api/all", function(data) {
 
 $(document).ready(function() {
   //Age search
-    $(".filter").on("click", function() {
-      criteria = this.id;
-      console.log("criteria");
-      console.log(criteria);
-  
-      $("#all-dogs").empty();
-      $.get("/api/" + criteria, function(data) {
-        console.log("DATA VVV");
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-          var wellSection = $("<div>");
-          wellSection.addClass("well");
-          wellSection.attr("id", "character-well-" + i);
-          $("#all-dogs").prepend(wellSection);
-          $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
-          $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
-          $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
-        }
-      });
+  $(".filter").on("click", function() {
+    criteria = this.id;
+    console.log("criteria");
+    console.log(criteria);
+
+    $("#all-dogs").empty();
+    $.get("/api/" + criteria, function(data) {
+      console.log("DATA VVV");
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "character-well-" + i);
+        $("#all-dogs").prepend(wellSection);
+        $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+        $("#character-well-" + i).append(
+          "<h3>Breed: " + data[i].breed + "</h4>"
+        );
+        $("#character-well-" + i).append(
+          "<img src= '" + data[i].pic + "' id='dog-pic'>"
+        );
+      }
+    });
+  });
 });
