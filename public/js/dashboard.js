b7623306ff9255711f543c1a6a4357cc3c8dbbd7
PHOENUX\Brandon <bdstevenson4225@aol.com>
2019-06-22 09:13:50 -0400

    trash

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -13,11 +13,11 @@ $.get("/api/all", function(data) {
 });
 
 $(document).ready(function() {
-  $(".filter").on("click", function() {
-    criteria = this.id;
-    $("#all-dogs").empty()
-    $.get("/api/" + criteria, function(data) {
-      console.log("DATA VVV");
+  //Age search
+  $("#age1").on("click", function() {
+    console.log("click!");
+    $("#all-dogs").empty();
+    $.get("/api/age/1", function(data) {
       console.log(data);
       for (var i = 0; i < data.length; i++) {
         var wellSection = $("<div>");
@@ -28,6 +28,178 @@ $(document).ready(function() {
         $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
         $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
       }
+    });
+  });
+
+  $("#age2").on("click", function() {
+    console.log("click!");
+    $("#all-dogs").empty();
+    $.get("/api/age/2", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "character-well-" + i);
+        $("#all-dogs").prepend(wellSection);
+        $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+        $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
+        $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
+      }
+    });
+  });
+
+  $("#age3").on("click", function() {
+    console.log("click!");
+    $("#all-dogs").empty();
+    $.get("/api/age/3", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "character-well-" + i);
+        $("#all-dogs").prepend(wellSection);
+        $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+        $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
+        $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
+      }
+    });
+  });
+
+  //Size search
+  $("#sz1").on("click", function() {
+    console.log("click!");
+    $("#all-dogs").empty();
+    $.get("/api/sz/1", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "character-well-" + i);
+        $("#all-dogs").prepend(wellSection);
+        $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+        $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
+        $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
+      }
+    });
+  });
+
+  $("#sz2").on("click", function() {
+    console.log("click!");
+    $("#all-dogs").empty();
+    $.get("/api/sz/2", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "character-well-" + i);
+        $("#all-dogs").prepend(wellSection);
+        $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+        $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
+        $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
+      }
+    });
+  });
+
+  $("#sz3").on("click", function() {
+    console.log("click!");
+    $("#all-dogs").empty();
+    $.get("/api/sz/3", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "character-well-" + i);
+        $("#all-dogs").prepend(wellSection);
+        $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+        $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
+        $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
+      }
+    });
+  });
+
+  //Personality Search
+  $("#pers1").on("click", function() {
+    console.log("click!");
+    $("#all-dogs").empty();
+    $.get("/api/pers/1", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "character-well-" + i);
+        $("#all-dogs").prepend(wellSection);
+        $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+        $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
+        $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
+      }
+    });
+  });
+
+  $("#pers2").on("click", function() {
+    console.log("click!");
+    $("#all-dogs").empty();
+    $.get("/api/pers/2", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "character-well-" + i);
+        $("#all-dogs").prepend(wellSection);
+        $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+        $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
+        $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
+      }
+    });
+  });
+
+  $("#pers3").on("click", function() {
+    console.log("click!");
+    $("#all-dogs").empty();
+    $.get("/api/pers/3", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "character-well-" + i);
+        $("#all-dogs").prepend(wellSection);
+        $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+        $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
+        $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
+      }
+    });
+  });
+
+  $("#pers4").on("click", function() {
+    console.log("click!");
+    $("#all-dogs").empty();
+    $.get("/api/pers/4", function(data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        var wellSection = $("<div>");
+        wellSection.addClass("well");
+        wellSection.attr("id", "character-well-" + i);
+        $("#all-dogs").prepend(wellSection);
+        $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+        $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
+        $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
+      }
+    });
+  });
+
+  // $(".filter").on("click", function() {
+  //   criteria = this.id;
+  //   // $("#all-dogs").empty()
+  //   $.get("/api/" + CRITRE, function(data) {
+  //     console.log(data);
+  //     for (var i = 0; i < data.length; i++) {
+  //       var wellSection = $("<div>");
+  //       wellSection.addClass("well");
+  //       wellSection.attr("id", "character-well-" + i);
+  //       $("#all-dogs").prepend(wellSection);
+  //       $("#character-well-" + i).append("<h2>" + data[i].dogName + "</h2>");
+  //       $("#character-well-" + i).append("<h3>Breed: " + data[i].breed + "</h4>");
+  //       $("#character-well-" + i).append("<img src= '" + data[i].pic + "' id='dog-pic'>");
+  //     }
     });
   });
 });
